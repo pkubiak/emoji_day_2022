2337bf22aa66fc783ce25f03498e18dd3ab439ce
Paweł Kubiak <pawel.kubiak@protonmail.com>
2022-09-07 05:15:45 +0200

    feat: Created script for dumping data as tsv

---
+++ data_extractor.py
@@ -0,0 +1,78 @@
+import os, json, gzip
+from collections import Counter, defaultdict
+import pandas as pd
+from utils import iterate_dates_range, chunk
+from tqdm import tqdm
+from typing import Iterable, Dict
+
+
+def extract_tweets(dir: str, date: str):
+    path = os.path.join(dir, f"{date}.jsonl")
+    if os.path.exists(path + ".gz"):
+        path += ".gz"
+    print(f"Opening: {path}")
+    with (gzip.open if path.endswith(".gz") else open)(path, "rb") as file:
+        for line in file:
+            tweet = json.loads(line.decode("utf-8"))
+            yield tweet
+
+
+
+class Collector:
+    def __ilshift__(self, tweets):
+        if not isinstance(tweets, list):
+            tweets = [tweets]
+
+        self.collect(tweets)
+
+        return self
+
+
+class TweetsPerHourCollector(Collector):
+    """Count number of Tweets in hourly intervals."""
+
+    def clear(self):
+        self.counts = Counter()
+        self.seconds = defaultdict(set)
+
+    def __init__(self):
+        self.clear()
+
+    def collect(self, tweets: Iterable[Dict]) -> None:
+        for tweet in tweets:
+            hour, rem  = tweet["data"]["created_at"].split(":", maxsplit=1)
+            m, s = map(int, rem.split(".")[0].split(":"))
+            secs = 60*m+s
+            self.counts[hour] += 1
+            self.seconds[hour].add(secs)
+
+    def df(self) -> pd.DataFrame:
+        return pd.DataFrame(
+            [k.split("T") + [v, round(100.0*len(self.seconds[k])/3600, 3)] for k, v in self.counts.items()],
+            columns=["Day", "Hour", "Count", "Coverage%"],
+        ).sort_values(by=["Day", "Hour"])
+
+
+if __name__ == "__main__":
+    collectors = {"tweets_per_hour": TweetsPerHourCollector()}
+
+    for date in iterate_dates_range("2022-09-02", "2022-09-06"):
+        print("=" * 16, date, "=" * 16)
+
+        for collector in collectors.values():
+            collector.clear()
+
+        with tqdm(extract_tweets("streams", date), leave=False) as it:
+            for tweets in chunk(it):
+                for collector in collectors.values():
+                    collector <<= tweets
+
+        for name, collector in collectors.items():
+            output_path = os.path.join("data", date, f"{name}.tsv")
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            df = collector.df()
+
+            print(f"Saving to {output_path}")
+            print(df)
+            df.to_csv(output_path, index=False, sep="\t")
+            
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from typing import Iterable, Dict
 import glob, json, os
 import emoji
+import pandas as pd
 
 Tweet = Dict
 
@@ -8,7 +9,7 @@ def iteratate_tweets(dirname: str, limit: int = 10000000000) -> Iterable[Tweet]:
     dirname = os.path.join(os.path.abspath(dirname), "*.jsonl")
     print(dirname)
 
-    for path in sorted(glob.glob(dirname)):
+    for path in sorted([]):
         with open(path,"r",encoding="utf-8") as file:
             print(path)
             ile = 0
@@ -19,6 +20,7 @@ def iteratate_tweets(dirname: str, limit: int = 10000000000) -> Iterable[Tweet]:
                 if ile >= limit:
                     break
             print(path, ile)
+
 
 def iterate_dates_range(start: str, end: str, as_str: bool = True) -> Iterable[str]:
     from datetime import datetime, timedelta
@@ -59,3 +61,22 @@ def render_as_table(items, n_cols: int = 6, limit: int = None) -> str:
     html += ["</table>"]
 
     return "\n".join(html)
+
+def get_tsv_data(key: str, **kwargs):
+    dfs = []
+    for path in glob.glob(os.path.join(os.path.dirname(__file__), "data","*",f"{key}.tsv")):
+        print(os.path.abspath(path))
+        df = pd.read_csv(path, sep="\t", **kwargs)
+        dfs.append(df)
+    return pd.concat(dfs, ignore_index=True)
+    
+
+def chunk(items, chunk_size: int = 1000):
+    cache = []
+    for item in items:
+        if len(cache) == chunk_size:
+            yield cache
+            cache = []
+        cache.append(item)
+
+    yield cache
